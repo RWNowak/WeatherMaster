36ce7de90654bceac64c7cb5ac171c54c493f152
Rafał Nowak <rwojciechnowak@gmail.com>
2023-05-28 13:20:10 +0200

    feat: begin creating app logic

--- app/src/main/java/com/example/weathermaster/MainActivity.java
+++ app/src/main/java/com/example/weathermaster/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.weathermaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etCity, etCountry;
+    TextView tvResult;
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "d12e8b6e5288f47ebedac28932cb67e4";
+    DecimalFormat df = new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCity = findViewById(R.id.etCity);
+        etCountry = findViewById(R.id.etCountry);
+        tvResult = findViewById(R.id.tvResult);
+
+    }
+
+    public void GetWeatherDetails(View view) {
+        String tempUrl = "";
+        String city = etCity.getText().toString().trim();
+        String country = etCountry.getText().toString().trim();
+        if(city.equals("")){
+            tvResult.setText("City field can not be empty!");
+        } else {
+            if(country.equals("")){
+                tempUrl = url + "?q=" + city + "," + country + "&appid=" + appid;
+            } else {
+                tempUrl = url + "?q=" + city + "&appid=" + appid;
+            }
     }
 }
